c9a003ce009573cb220aa766e24520407012f1a2
Taslim A Hussain <taslim@outlook.co.nz>
2024-03-09 14:06:24 +1300

    update 	modified:   bin/apicall/user.js

--- bin/apicall/user.js
+++ bin/apicall/user.js
@@ -6,7 +6,7 @@ const resource = "user";
 // Login
 export const loginCall = async (pass) => {
 	spinner.start("Logging in...");
-	const url = `${resource}/login`;
+	const url = `${resource}/cli/login`;
 	return await makeAPICallWithRetries("post", spinner, null, url, pass);
 };
 
@@ -20,6 +20,6 @@ export const getUserCall = async (token) => {
 // Logout
 export const logoutCall = async (token) => {
 	spinner.start("Logging out...");
-	const url = `${resource}/logout`;
+	const url = `${resource}/cli/logout`;
 	return await makeAPICallWithRetries("post", spinner, token, url);
 };
